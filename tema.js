949512c33195c169ebfcaa2e1948166b6771fb79
Dincă Silviu Cristian <silviudinca412@gmail.com>
2021-10-25 10:53:47 +0300

    Add files via upload

---
+++ tema.js
@@ -0,0 +1,20 @@
+window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_orientation(evt) {
+    let alpha = evt.alpha;
+    let beta = evt.beta;
+    let gamma = evt.gamma;
+
+    document.getElementById("a").innerHTML = "alpha = "+alpha;
+    document.getElementById("b").innerHTML = "beta = "+beta;
+    document.getElementById("c").innerHTML = "gamma = "+gamma;
+
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    let centru = {x:canvas.width/2, y:canvas.height/2}
+    let latura_patrat = 50;
+    context.clearRect(0, 0, c.width, c.height);
+    context.fillStyle = "#6A0888";
+    context.beginPath();
+    context.fillRect(180, 150, gamma, 50)
+}
