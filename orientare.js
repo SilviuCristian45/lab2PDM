9855397713e97da83f66ebc6a91fd7a89a5799ad
Dincă Silviu Cristian <silviudinca412@gmail.com>
2021-10-25 10:30:40 +0300

    Add files via upload

---
+++ orientare.js
@@ -0,0 +1,26 @@
+window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_orientation(event) {
+    //preluam valorile unghiurilor
+    let alpha = evt.alpha;
+    let beta = evt.beta;
+    let gamma = evt.gamma;
+    //le afisam in paragrafe
+    document.getElementById('a').innerHTML = "alpha = " + alpha;
+    document.getElementById('b').innerHTML = "beta = " + alpha;
+    document.getElementById('c').innerHTML = "gamma = " + alpha;
+    
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext('2d');
+    let centru = {x:canvas.width/2,
+                  y:canvas.height/2};
+    let latura_patrat = 50;
+
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0,0, canvas.width, canvas.height);
+    context.translate(centru.x/2, centru.y/2);
+    context.rotate(alpha * Math.PI / 180);
+    context.beginPath();
+    context.rect(-latura_patrat/2, -latura_patrat/2, latura_patrat, latura_patrat);
+    context.stroke();
+}
